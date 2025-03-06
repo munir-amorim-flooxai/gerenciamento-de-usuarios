1eb398ff171583a3587e1c8284f87311134407d7
munir-amorim-flooxai <munir.amorim.flooxai@gmail.com>
2025-03-06 12:48:41 -0300

    feat: add event handling for form submission and store data in JSON

--- curso-javascript-projeto-usuarios-master/index.js
+++ curso-javascript-projeto-usuarios-master/index.js
@@ -1,20 +1,26 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-fields.forEach(function(field, index){
+document.getElementById("form-user-create").addEventListener("submit", function(event){
 
-    if (field.name == "gender"){
+    event.preventDefault();
 
-        if (field.checked){
+    fields.forEach(function(field, index){
+
+        if (field.name == "gender"){
+    
+            if (field.checked){
+                user[field.name] = field.value;
+            }        
+    
+        }else {
+    
             user[field.name] = field.value;
-        }        
-
-    }else {
-
-        user[field.name] = field.value;
-
-    }
+    
+        }
+    
+    });
+    
+    console.log(user);
 
 });
-
-console.log(user);
