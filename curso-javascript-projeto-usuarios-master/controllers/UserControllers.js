f7e74d2eb4f25ae9e0ce97a1623b03a898a70e0c
munir-amorim-flooxai <munir.amorim.flooxai@gmail.com>
2025-03-10 12:31:39 -0300

    feat: implement functions to improve code modularity and reusability

---
+++ curso-javascript-projeto-usuarios-master/controllers/UserControllers.js
@@ -0,0 +1,78 @@
+class UserControllers {
+
+    constructor(formId, tableId){
+
+        this.formIdEl = document.getElementById(formId);
+        this.formIdEl = document.getElementById(tableId);
+
+        this.onSubmit();
+
+    }
+
+    onSubmit(){
+
+        this.formIdEl.addEventListener("submit", event =>{
+
+            event.preventDefault();
+
+            this.addLine(this.getValues());
+        
+        });
+
+    }
+
+    getValues(){
+
+        let user = {};
+
+        this.formIdEl.elements.forEach(function(field, index){
+
+            if (field.name == "gender"){
+        
+                if (field.checked){
+                    user[field.name] = field.value;
+                }        
+        
+            }else {
+        
+                user[field.name] = field.value;
+        
+            }
+        
+        });
+    
+        return new User(
+            user.name,
+            user.gender,
+            user.birth,
+            user.country,
+            user.email,
+            user.password,
+            user.photo,
+            user.admin,
+    
+        );
+
+        
+
+    }
+
+    addLine(dataUser){
+    
+        this.tableEl.innerHTML =`
+        <tr>
+             <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+             <td>${dataUser.name}</td>
+             <td>${dataUser.email}<</td>
+             <td>${dataUser.admin}<</td>
+             <td>${dataUser.birth}<</td>
+             <td>
+                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+             </td>
+          </tr> 
+        `;
+    
+    }
+
+}
